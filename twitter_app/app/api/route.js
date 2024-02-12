d31934782dc0242f028325113f4ccfa1c7e68302
Nazim Uddin <107185368+Nazim6269@users.noreply.github.com>
2024-02-12 22:52:42 +0600

    delete twitter-app directory

--- twitter_app/app/api/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-    return new NextResponse(JSON.stringify({ welcome: 'Coders vaira amar!Server kam kortache ,r test korte hoibona,code kortejaow!' }), {
-      status: 401
-    })
-}
