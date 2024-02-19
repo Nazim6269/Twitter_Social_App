419237910afb34a77ef9eed7de9e14dc9a027167
Nazim Uddin <107185368+Nazim6269@users.noreply.github.com>
2024-02-19 19:33:02 +0600

    added signup route

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,10 +1,23 @@
 const Login = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center ">
-      <form className="bg-gray-200 w-3/5 px-8 py-6 ">
-        <h2 className="text-3xl mb-3 text-center font-semibold leading-7 text-gray-900">
-          Login
+    <div className="min-h-screen flex justify-center items-center">
+      <form className="bg-gray-200 w-3/5 px-8 py-6">
+        <h2 className="capitalize text-3xl mb-3 text-center font-semibold leading-7 text-gray-900">
+          signup
         </h2>
+        {/* username */}
+        <div className="">
+          <label
+            for="username"
+            className="block capitalize text-md font-medium leading-6 text-gray-900"
+          >
+            username
+          </label>
+          <input
+            type="text"
+            className="block flex-1 w-full border-0 bg-transparent py-1.5 pl-1 my-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+          />
+        </div>
 
         {/* gamail  */}
         <div>
@@ -35,11 +48,11 @@ const Login = () => {
 
         <button
           type="submit"
-          className="rounded-md w-full bg-blue-400 px-3 py-2 text-sm font-semibold text-gray shadow-sm"
+          className="capitalize rounded-md w-full bg-blue-400 px-3 py-2 text-sm font-semibold text-gray shadow-sm"
         >
-          Login
+          signup
         </button>
-        <p className="underline text-blue-600 mt-3">create an account</p>
+        <p className="underline text-blue-600 mt-3">already have an account?</p>
         <p className="underline text-blue-600 mt-3">forgot password?</p>
       </form>
     </div>
