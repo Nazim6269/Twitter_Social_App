a306f321ff07857f8e34dd93421d572c133e6b3a
Nazim Uddin <107185368+Nazim6269@users.noreply.github.com>
2024-02-19 19:50:28 +0600

    added some css in signup and logout page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,8 +1,8 @@
 const Login = () => {
   return (
     <div className="min-h-screen flex justify-center items-center ">
-      <form className="bg-gray-200 w-3/5 px-8 py-6 ">
-        <h2 className="text-3xl mb-3 text-center font-semibold leading-7 text-gray-900">
+      <form className="bg-gray-800 shadow-lg rounded-md  w-3/5 px-8 py-6 ">
+        <h2 className="text-3xl mb-3 text-center font-semibold leading-7 text-white">
           Login
         </h2>
 
@@ -10,26 +10,26 @@ const Login = () => {
         <div>
           <label
             for="username"
-            className="block capitalize text-md font-medium leading-6 text-gray-900"
+            className="block capitalize text-md font-medium leading-6 text-white"
           >
             gmail
           </label>
           <input
             type="text"
-            className="block flex-1 w-full border-0 bg-transparent py-1.5 pl-1 my-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+            className="block flex-1 w-full border-0 bg-gray-900 py-1.5 pl-1 my-2 text-gray-200 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
           />
         </div>
         {/* password */}
         <div>
           <label
             for="username"
-            className="block capitalize text-md font-medium leading-6 text-gray-900"
+            className="block capitalize text-md font-medium leading-6 text-white"
           >
             password
           </label>
           <input
             type="password"
-            className="block flex-1 w-full border-0 bg-transparent py-1.5 pl-1 my-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+            className="block flex-1 w-full border-0 bg-gray-900 py-1.5 pl-1 my-2 text-gray-200 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
           />
         </div>
 
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,21 +1,21 @@
 const Login = () => {
   return (
     <div className="min-h-screen flex justify-center items-center">
-      <form className="bg-gray-200 w-3/5 px-8 py-6">
-        <h2 className="capitalize text-3xl mb-3 text-center font-semibold leading-7 text-gray-900">
+      <form className="bg-gray-800 shadow-lg rounded-md w-3/5 px-8 py-6">
+        <h2 className="capitalize text-3xl mb-3 text-center font-semibold leading-7 text-white">
           signup
         </h2>
         {/* username */}
         <div className="">
           <label
             for="username"
-            className="block capitalize text-md font-medium leading-6 text-gray-900"
+            className="block capitalize text-md font-medium leading-6 text-white"
           >
             username
           </label>
           <input
             type="text"
-            className="block flex-1 w-full border-0 bg-transparent py-1.5 pl-1 my-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+            className="block flex-1 w-full border-0 bg-gray-900 py-1.5 pl-1 my-2 text-white placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
           />
         </div>
 
@@ -23,26 +23,26 @@ const Login = () => {
         <div>
           <label
             for="username"
-            className="block capitalize text-md font-medium leading-6 text-gray-900"
+            className="block capitalize text-md font-medium leading-6  text-white"
           >
             gmail
           </label>
           <input
             type="text"
-            className="block flex-1 w-full border-0 bg-transparent py-1.5 pl-1 my-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+            className="block flex-1 w-full border-0 bg-gray-900 py-1.5 pl-1 my-2  text-white placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
           />
         </div>
         {/* password */}
         <div>
           <label
             for="username"
-            className="block capitalize text-md font-medium leading-6 text-gray-900"
+            className="block capitalize text-md font-medium leading-6  text-white"
           >
             password
           </label>
           <input
             type="password"
-            className="block flex-1 w-full border-0 bg-transparent py-1.5 pl-1 my-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+            className="block flex-1 w-full border-0 bg-gray-900 py-1.5 pl-1 my-2 text text-white placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
           />
         </div>
 
